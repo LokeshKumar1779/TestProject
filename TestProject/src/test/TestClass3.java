6eee813b29df598504df01a42fa1bceb9d894fcd
LokeshKumar1779 <kumarlokesh57@gmail.com>
2022-03-15 15:07:42 +0530

    fg

--- TestProject/src/test/TestClass3.java
+++ TestProject/src/test/TestClass3.java
@@ -3,11 +3,11 @@ package test;
 public class TestClass3 {
 	
 	public void name() {
-<<<<<<< HEAD
+
 		System.out.println("this class is added in dev branch");
-=======
+
 		System.out.println("this class is added in master branch");
->>>>>>> refs/remotes/origin/dev
+
 	}
 
 }
